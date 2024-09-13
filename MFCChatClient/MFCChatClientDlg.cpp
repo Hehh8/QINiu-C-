8832934e4c4db6444019fee1f7f17008d6c8194d
Hehh8 <2424952886@qq.com>
2024-09-13 15:47:28 +0800

    尝试添加DebugView

--- MFCChatClient/MFCChatClientDlg.cpp
+++ MFCChatClient/MFCChatClientDlg.cpp
@@ -65,6 +65,7 @@ BEGIN_MESSAGE_MAP(CMFCChatClientDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_CONNECT_BUT, &CMFCChatClientDlg::OnClickedConnectBut)
 END_MESSAGE_MAP()
 
 
@@ -153,3 +154,11 @@ HCURSOR CMFCChatClientDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CMFCChatClientDlg::OnClickedConnectBut()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	TRACE("[ChatClient]Connent was clicked");
+	MessageBoxW(L"Connent was clicked");
+}
